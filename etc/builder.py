7dcb2e8cfead75b3fd767b75eaf36d9caa0c3944
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2012-06-13 16:22:31 +0000

    laudaRE2xx: moved new ramp stuff to new files until controller is working

--- etc/builder.py
+++ etc/builder.py
@@ -1,7 +1,9 @@
 from iocbuilder import AutoSubstitution
 from iocbuilder.modules.streamDevice import AutoProtocol
+from iocbuilder.modules.busy import Busy
 
 class lRE2xx(AutoSubstitution, AutoProtocol):
+    Dependencies = (Busy,)
     # Substitution attributes
     TemplateFile = 'lRE2xx.template'
 
